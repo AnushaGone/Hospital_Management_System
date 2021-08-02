4f63c7d60b8d0628e8e6d80c899bd77463a00a2e
AnushaGone <83800345+AnushaGone@users.noreply.github.com>
2021-08-02 09:50:06 +0530

    Delete SAS_HospitalManagement directory

--- SAS_HospitalManagement/src/main/java/com/inno/SAS_HospitalManagement/model/Admin.java
+++
@@ -1,5 +0,0 @@
-package com.inno.SAS_HospitalManagement.model;
-
-public class Admin{
-
-}
--- SAS_HospitalManagement/src/main/java/com/inno/SAS_HospitalManagement/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.inno.SAS_HospitalManagement.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.inno.SAS_HospitalManagement.model.Patient;
-
-public interface PatientRepository extends CrudRepository<Patient, String> {
-	
-}
